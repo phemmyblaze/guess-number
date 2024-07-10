b142c2402e5728fdb890f0cc5dce40a9b5988f8a
Akinuliola Akinbobola Oluwafemi <71355010+phemmyblaze@users.noreply.github.com>
2024-07-10 11:40:15 +0100

    guess your number

---
+++ index.js
@@ -0,0 +1,28 @@
+// document.querySelector(".check").addEventListener("click", function () {
+// 	document.querySelector(".message").textContent = "My name is Phemmyblaze";
+// 	document.querySelector(".message").style.fontSize = "50rem";
+// 	console.log("Button clicked");
+// });
+const number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".check").addEventListener("click", function () {
+	const guess = Number(document.querySelector(".guess").value);
+
+	if (!guess) {
+		document.querySelector(".message").textContent = "No number was inputted";
+	} else if (guess === number) {
+		document.querySelector(".message").textContent = "You guessed the correct number";
+		document.querySelector(".number").textContent = number;
+		document.querySelector("body").style.backgroundColor = " green";
+	} else if (guess > number) {
+		document.querySelector(".message").textContent = "You guessed an higher number";
+		//score --
+		score = score - 1;
+		document.querySelector(".score").textContent = score;
+	} else if (guess < number) {
+		document.querySelector(".message").textContent = "You guessed an lesser number";
+		score = score - 1;
+		document.querySelector(".score").textContent = score;
+	}
+});
